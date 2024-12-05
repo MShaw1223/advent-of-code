9d90841c86d4578859b5e5f73514fdd35a60eb22
Miller <millershaw61@gmail.com>
2024-12-05 01:35:29 +0000

    day-three pt2 NOT FINISHED
    Pulled logic into a seperate function for pt1 and pt2 to clean up what to focus on.

--- src/bin/day-three.rs
+++ src/bin/day-three.rs
@@ -6,59 +6,43 @@ fn main() {
     //part_one();
     part_two();
 }
+fn process_mul(line: String) -> i32 {
+    let re = Regex::new(r"mul\((\d+),(\d+)\)+").expect("Regex creation failed");
+    let mut line_sum = 0;
 
+    for cap in re.captures_iter(&line) {
+        let first_val = cap.get(1).unwrap().as_str();
+        let second_val = cap.get(2).unwrap().as_str();
+        let fv_integer: i32 = first_val.parse().unwrap();
+        let sv_integer: i32 = second_val.parse().unwrap();
+
+        let sum_mul: i32 = fv_integer * sv_integer;
+        line_sum += sum_mul;
+    }
+
+    line_sum
+}
 fn part_one() {
-    let mut sum: i32 = 0;
-
+    let mut sum = 0;
     for line in std::io::stdin().lock().lines() {
-        let re = Regex::new(r"mul\((\d+),(\d+)\)+");
         match line {
-            Ok(line) => {
-                for cap in re.expect("Func Regex Error").captures_iter(&line) {
-                    let first_val = cap.get(1).unwrap().as_str();
-                    let second_val = cap.get(2).unwrap().as_str();
-                    let fv_integer: i32 = first_val.parse().unwrap();
-                    let sv_integer: i32 = second_val.parse().unwrap();
-
-                    let sum_mul: i32 = fv_integer * sv_integer;
-
-                    sum += sum_mul
-                }
-            }
+            Ok(line) => sum += process_mul(line),
             Err(e) => println!("Error: {e:?}"),
         }
     }
-    println!("{sum}")
+    println!("Sum: {sum}")
 }
-// not working yet
-fn part_two() {
-    let mut act: bool = true;
 
+fn part_two() {
     let mut sum: i32 = 0;
-    let re = Regex::new(r"mul\((\d+),(\d+)\)+");
-    let re_do = Regex::new(r"do\(\)").unwrap();
-    let re_do_not = Regex::new(r"don't\(\)").unwrap();
+    let re_do = Regex::new(r"^do\(\)").unwrap();
+    //let re_do_not = Regex::new(r"don't\(\)").unwrap();
 
     for line in std::io::stdin().lock().lines() {
         match line {
             Ok(line) => {
                 if re_do.is_match(&line) {
-                    act = true
-                } else if re_do_not.is_match(&line) {
-                    act = false
-                }
-
-                if act {
-                    for cap in re.clone().expect("re Regex Error").captures_iter(&line) {
-                        let first_val = cap.get(1).unwrap().as_str();
-                        let second_val = cap.get(2).unwrap().as_str();
-                        let fv_integer: i32 = first_val.parse().unwrap();
-                        let sv_integer: i32 = second_val.parse().unwrap();
-
-                        let sum_mul: i32 = fv_integer * sv_integer;
-
-                        sum += sum_mul
-                    }
+                    sum += process_mul(line)
                 }
             }
             Err(e) => println!("Error: {e:?}"),
